409ea8c104902433113573a7f4829228d6d6aebe
daGodz <uribae@protonmail.com>
2022-09-26 12:29:31 -0400

    jgjg

--- components/Book.jsx
+++ components/Book.jsx
@@ -21,7 +21,6 @@
             <input name="bot-field" />
           </div>
   
-  
           <div class="relative pt-4">
             <label
               for="name"
@@ -43,31 +42,21 @@
             </label>
           </div>
   
-
-
-
-
-
-          <div class="inline-block relative pt-6 pb-6 w-64">
-          <label class='font-bold text-red' htmlFor="lashes">Will you join our blast off?</label>
-          <select type="lashes" htmlFor="lashes" id="lashes" name="lashes" class="block appearance-none w-full bg-wood border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-    <option type="lashes" htmlFor="lashes" id="lashes" name="lashes">I'll be there (: </option>
-    <option type="lashes" htmlFor="lashes" id="lashes" name="lashes">Cant make it!</option> 
-    <option type="lashes" htmlFor="lashes" id="lashes" name="lashes">Will Try!</option> 
-    
-  </select>
-  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-  </div>
+<div className="pt-12">
+<a className="text-red-500 font-bold text-2xl pb-12">Will you be joining Izar's space adventure?</a>
 </div>
       
-      <div class="flex items-center mb-4">
+      <div class="flex items-center mb-4 pt-12">
     <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
+    <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes! Count me in!</label>
     </div>
 <div class="flex items-center">
     <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
+    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">No, but have a blast!</label>
+</div>
+<div class="flex items-center">
+    <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 pt-3 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Maybe! See you at blastoff!</label>
 </div>
 
       
@@ -75,7 +64,7 @@
         <button
         alert-message="Message"
           type="submit"
-          class="focus:shadow-outline w-full transform rounded-md border-indigo-600 bg-wood py-3 text-base text-black ring-offset-2 ring-offset-current transition duration-500 ease-in-out hover:bg-blue-800 focus:outline-none focus:ring-2 "
+          class=" w-full transform rounded-md bg-white py-3 text-base text-black ring-offset-2 ring-offset-current transition duration-500 ease-in-out hover:bg-blue-800 focus:outline-none focus:ring-2 "
         >
           {' '}
           Confirm!
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,11 @@ const Home: NextPage = () => {
   return (
     <div className='bg-black'>
 
-<div className='animate-bounce pt-12'>
+<div className='animate-bounce pt-12 flex flex-row'>
   <Image src="/moon.png" width="200" height="200" />
+  <Image src="/sun.png" width="250" height="250" />
 </div>
+
     <div className="flex flex-col items-center justify-center  bg-black text-white">
       <Head>
         <title>Izar One Year Bash!</title>
@@ -17,28 +19,30 @@ const Home: NextPage = () => {
 
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-black">
-        <h1 className="text-3xl font-bold pb-4">
+        <h1 className="text-3xl font-bold pb-4 italic">
           We Invite You To Celebrate{' '}
           
         </h1>
 
         <a className="text-blue-600  animate-pulse text-4xl font-bold pt-3">
-            Izar's One Year Birthday!
+            Izar's One Year Around The Sun!
           </a>
 
-        <p className="mt-3 text-1xl text-blue pt-6 underline" >
+        <p className="mt-3 text-1xl text-blue pt-6 underline italic" >
           Oct 8th 4PM
           
         </p>
-        <p className="mt-3 text-1xl pt-3">
+        <p className="mt-3 text-1xl pt-3 underline-offset-2 italic">
           
           2304 Hunter Davis CT Monroe NC 28110
         </p>
-<div className='animate-bounce'>
-  <Image src="/star.png" width="200" height="200" />
-</div>
+
        
       </main>
+</div>
+<div className='animate-bounce pt-12 flex flex-row'>
+  <Image src="/space.png" width="200" height="200" />
+  <Image src="/star.png" width="250" height="250" />
 </div>
 <Book />
 </div>
